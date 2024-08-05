2d44becaa5de76ddbbd81e67835c8dca5d296f67
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2024-08-05 13:41:48 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("testando..");
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var var1 = 145,
+  var2 = 2,
+  var3 = 3,
+  var4 = 4;
+
+  console.log(var1, var2, var3);
+
